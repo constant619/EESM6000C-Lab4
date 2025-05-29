ea603387dd1ae1f52c7d8071b463fe98b287c8d3
constant619 <ylitp@connect.ust.hk>
2025-05-29 19:30:16 +0800

    Update fir.c

--- Lab 4-1/testbench/counter_la_fir/fir.c
+++ Lab 4-1/testbench/counter_la_fir/fir.c
@@ -2,11 +2,23 @@
 
 void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
 	//initial your fir
+	for(int i=0; i<N; i++){
+		inputbuffer[i]=0;
+		outputsignal[i]=0;
+	}
 }
 
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
 	initfir();
 	//write down your fir
+	for(int i=0; i<N; i++){
+		inputbuffer[i]=inputsignal[i];
+		for(int j=0; j<N; j++){
+			if((i-j)>=0){
+				outputsignal[i]+=taps[i]*inputbuffer[i-j];
+			}
+		}
+	}
 	return outputsignal;
 }
 		
